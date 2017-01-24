f8e13b685b60980a4c8b89150023b135f5922ea1
Mike Mayers <mmayers@scripps.edu>
2017-01-24 12:15:25 -0800

    Blazgraph supports .gz bulk loading

--- download_data.sh
+++ download_data.sh
@@ -2,4 +2,4 @@
 
 mkdir data
 cd data
-wget https://dumps.wikimedia.org/wikidatawiki/entities/20170116/wikidata-20170116-all-BETA.ttl.bz2
+wget https://dumps.wikimedia.org/wikidatawiki/entities/20170116/wikidata-20170116-all-BETA.ttl.gz
