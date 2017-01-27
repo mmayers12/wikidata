1eefa0cbdde995a7cc97d6b3c0ce21f52347b7b8
Mike Mayers <mmayers@scripps.edu>
2017-01-27 13:25:35 -0800

    rebuild the database from the .gz

--- install_blazegraph.sh
+++ install_blazegraph.sh
@@ -8,3 +8,9 @@ git clone -b BLAZEGRAPH_RELEASE_$RELEASE --single-branch https://github.com/blaz
 # Build Blazegraph
 cd BLAZEGRAPH_RELEASE_$RELEASE
 ./scripts/mavenInstall.sh
+
+# Configure the properties to dump to the wikidata.jnl file
+echo -e '\n# Persistence Store File\ncom.bigdata.journal.AbstractJournal.file=wikidata.jnl' >> build.properties
+
+# Run DataLoader script to rebuild the database
+./scripts/dataLoader.sh -format Turtle build.properties ../data/wikidata-20170116-all-BETA.ttl.gz
