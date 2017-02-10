43d7cf3e488853731379bfad6167f889ca3da880
Mike <mmayers@avalanche.scripps.edu>
2017-02-10 14:21:14 -0800

    fix error in data directory

--- clean_data.sh
+++ clean_data.sh
@@ -8,5 +8,5 @@ cd db
 # Begin Process of cleaning the data
 # Script will remvoe non-english entries, split into managable sized files
 # And remove sitelinks
-nohup ./munge.sh -f ../data/$DATAFILE -d ../data/split -l en -s &> ../dataclean.log &
+nohup ./munge.sh -f ../$DATAFILE -d ../data/split -l en -s &> ../dataclean.log &
 
