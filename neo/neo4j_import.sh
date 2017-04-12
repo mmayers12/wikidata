9b6a3b23fabe61ddbef2bbe57e4098af59a5465b
Mike Mayers <mmayers@scripps.edu>
2017-04-12 12:12:46 -0700

    bug fixes for importing to neo4j

--- neo/neo4j_import.sh
+++ neo/neo4j_import.sh
@@ -1,17 +1,16 @@
 VERSION="neo4j-community-3.1.3"
-
 DB_NAME="wikidata-v0.1"
 
 declare -a suffixes=("" "_perm-1" "_perm-2" "_perm-3" "_perm-4" "_perm-5")
 
 for suffix in "${suffixes[@]}"; do
 
-    BIN_LOC="neo/${VERSION}_$DB_NAME$suffix/bin/neo4j-admin"
+    BIN_LOC="${VERSION}_$DB_NAME$suffix/bin/neo4j-admin"
 
-    CSV_NAME="nbs/data/permuted/hetnet_basedges$suffix"
+    CSV_NAME="import/hetnet_baseedges$suffix"
 
     REPORT_NAME="network${suffix}_import.report"
 
-    ./$BIN_LOC import --nodes "nbs/data/hetnet_basenodes.csv" --relationships "${CSV_NAME}.csv" --report-file="$REPORT_NAME"
+    ./$BIN_LOC import --nodes "import/hetnet_basenodes.csv" --relationships "${CSV_NAME}.csv" --report-file="$REPORT_NAME"
 
 done
