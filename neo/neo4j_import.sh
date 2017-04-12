f39ceb47ceb9605632082af015c903225a9bad9b
Mike Mayers <mmayers@scripps.edu>
2017-04-12 11:42:36 -0700

    Automate neo4j download and import pipeline

--- neo/neo4j_import.sh
+++ neo/neo4j_import.sh
@@ -2,7 +2,7 @@ VERSION="neo4j-community-3.1.3"
 
 DB_NAME="wikidata-v0.1"
 
-declare -a suffixes=("_perm-1" "_perm-2" "_perm-3" "_perm-4" "_perm-5")
+declare -a suffixes=("" "_perm-1" "_perm-2" "_perm-3" "_perm-4" "_perm-5")
 
 for suffix in "${suffixes[@]}"; do
 
