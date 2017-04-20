a73be14ef202bc6dfa3665045821550361fe5948
Mike Mayers <mmayers@scripps.edu>
2017-04-20 11:46:02 -0700

    Put together pipeline of filtering low-count edges and reomving recpricoal relations before Neo4j import

--- py/processing.py
+++ py/processing.py
@@ -113,9 +113,9 @@ def combine_multiclass(df, edge_type="instance of", sep='; '):
     return df_out
 
 
-def class_mapp_dict(df, label = 'label'):
+def get_node_type_dict(df, label = 'label'):
     """
-    Returns a mapping dictionary from node URI to the node's class
+    Returns a mapping dictionary from node URI to the node's type
 
     :param df: Pandas.DataFrame, with column 's' containing node URIs and column 'label' class names
     :param label: String, alternate names column containing class names
@@ -168,11 +168,10 @@ def nodes_neo_export(edge_df):
     :return: Pandas.DataFrame, formatted for neo4j, import ready to be exported to csv
     """
     # Get a mapping from uri to type
-    type_dict = edge_df.set_index('s')['type'].to_dict()
+    type_dict = get_node_type_dict(edge_df, 'type')
 
     # Filter for subject nodes connected to other subject nodes, and only keep unique
-    subject_uri = list(set(edge_df['s']))
-    filt_edges = edge_df.query('o in {!r}'.format(subject_uri))
+    filt_edges = filter_untyped_nodes(edge_df)
     subj_nodes = filt_edges.drop_duplicates(subset='s')
     subj_nodes = subj_nodes.reset_index(drop=True)
 
@@ -191,39 +190,196 @@ def nodes_neo_export(edge_df):
 
     return node_out
 
+def filter_untyped_nodes(edge_df):
+    """
+    Filter out edges that point to objects not in the list of subjects
+
+    :param edge_df:  Pandas.DataFrame, with edges to be added to the graph
+    :return: Pandas.DataFrame, with bad edges removed
+    """
+    subject_uri = list(set(edge_df['s']))
+    filt_edges = edge_df.query('o in {!r}'.format(subject_uri))
+
+    return filt_edges
 
 
-def edges_neo_export(edge_df):
+def get_edge_types(edge_df):
+    node_type_dict = get_node_type_dict(edge_df, 'type')
+    abbrev_dict = get_abbrev_dict(edge_df)
+
+    def get_edge_type(row):
+        # Get types and abbreviations for edge start type and end type
+        start_type = node_type_dict[row['s']]
+        end_type = node_type_dict[row['o']]
+        edge_type = row['pLabel'].replace(' ', '-')
+
+        start_abbrev = abbrev_dict[start_type]
+        end_abbrev = abbrev_dict[end_type]
+        edge_abbrev = abbrev_dict[edge_type]
+
+        # Example 'Compound' -physically interacts with-> 'Protein' becomes:
+        # 'physically-interacts-with_CpiwP'
+        return edge_type + '_' + start_abbrev + edge_abbrev + end_abbrev
+
+    return edge_df.apply(get_edge_type, axis=1)
+
+
+def remove_low_count_edges(edge_df, cutoff=.1):
+    """
+    Removes the low count edges from the edge dataframe
+
+    :param edge_df:
+    :param cutoff:
+    :return:
+    """
+    # Make sure edges have been typed, if not, type them
+    if 'e_type' not in edge_df:
+        edge_df['e_type'] = get_edge_types(edge_df)
+
+    # Get counts for edge and node types
+    edge_type_counts = edge_df['e_type'].value_counts()
+    node_type_counts = edge_df['type'].value_counts()
+
+    # Generate an edge to node mapping
+    edge_to_node_mapping = edge_df.set_index('e_type')['type'].to_dict()
+
+    # Find the valid edges
+    valid_edges = []
+    for edge_type, count in edge_type_counts:
+        node_type = edge_to_node_mapping[edge_type]
+        # Keep if there are more than the cuttoff percentage of nodes
+        if count >= cutoff * node_type_counts[node_type]:
+            valid_edges.append(edge_type)
+
+    return edge_df.query('e_type in {}'.format(valid_edges)).reset_index(drop=True)
+
+
+def get_pair_lists(edge_df):
+    """
+    Takes edge list dataframe and converts to a dictionary of pair lists
+
+    :param edge_df: pandas.DataFrame edge list with node and edge types included
+    :return: Dictionary, key is edge type, value, list of tuples, URIs of start and end nodes for given edge
+    """
+    # Get edge types
+    edge_types = set(edge_df['e_type'])
+
+    # Iterate over types and get a pair list for each
+    pair_list_dict = dict()
+    for kind in edge_types:
+        edges_of_type = edge_df[edge_df['e_type'] == kind]
+
+        start = list(edges_of_type['s'])
+        end = list(edges_of_type['o'])
+        pair_list = [(s, e) for s, e in zip(start, end)]
+
+        pair_list_dict[kind] = pair_list
+
+    return pair_list_dict
+
+
+def invert_pairs(pair_list):
+    return [(b, a) for (a, b) in pair_list]
+
+
+def calc_overlap(pair_list, invert_list, cutoff=.9):
+    count_a, count_b = 0, 0
+
+    # if there is a large difference in size, don't bother checking
+    if (len(pair_list) / len(invert_list) < cutoff or
+            len(invert_list) / len(pair_list) < cutoff):
+        return 0
+
+    # Check both directions
+    for pair in pair_list:
+        if pair in invert_list:
+            count_a += 1
+
+    for pair in invert_list:
+        if pair in pair_list:
+            count_b += 1
+
+    # Return the minimum overlap
+    return min(count_a / len(pair_list), count_b / len(invert_list))
+
+
+def find_reciprocal_relations(pair_list, cutoff=0.9):
+    invert_list = {key: invert_pairs(value) for key, value in pair_list.items()}
+    kinds = list(pair_list.keys())
+
+    reciprocal_types = []
+
+    # Only compare 2 different relationship types once
+    for i, kind in enumerate(kinds):
+        for idx in range(i + 1, len(kinds)):
+            overlap = calc_overlap(pair_list[kind], invert_list[kinds[idx]])
+            if overlap > cutoff:
+                reciprocal_types.append([kind, kinds[idx]])
+
+    return reciprocal_types
+
+
+def remove_reciprocals(edge_df, reciprocal_types):
+    edge_copy = edge_df.copy()
+
+    for types in reciprocal_types:
+        # Make a copy of the subsection
+        swapped = edge_copy.loc[edge_df['e_type'] == types[1]].copy()
+
+        # Swap URIs and Labels
+        tmp_uri = swapped['s'].copy()
+        tmp_label = swapped['sLabel'].copy()
+        swapped['s'] = swapped['o']
+        swapped['sLabel'] = swapped['oLabel']
+        swapped['o'] = tmp_uri
+        swapped['oLabel'] = tmp_label
+
+        # Change the edge type
+        swapped['e_type'] = types[0]
+
+        # Apply the changes
+        edge_copy.loc[edge_df[':TYPE'] == types[1]] = swapped
+
+    return edge_copy.drop_duplicates()
+
+
+def prep_for_export(edge_df):
     """
     Takes a list of edges, and returns a csv of the edges, formatted for Neo4j import.
 
     :param edge_df: Pandas.DataFrame, with the edges to be added to the graph
     :return: Pandas.DataFrame, formatted for neo4j, import ready to be exported to csv
     """
-    type_dict = edge_df.set_index('s')['type'].to_dict()
-    subject_uri = list(set(edge_df['s']))
-    filt_edges = edge_df.query('o in {!r}'.format(subject_uri))
 
-    # Create an abbreviation dict for fast mapping
-    types = list(set(edge_df['type']))
-    abbrevs = [''.join([w[0].upper() for w in t.split(' ')]) for t in types]
-    abbrev_dict = {t:a for t,a in zip(types, abbrevs)}
+    # Filter the edges
+    filt_edges = filter_untyped_nodes(edge_df)
+    # Get their types
+    filt_edges['e_type'] = get_edge_types(filt_edges)
+    # Remove the edges with low counts
+    filt_edges = remove_low_count_edges(filt_edges)
 
-    def get_edge_type(row):
-        # Get abbreviaton for edge start type and end type
-        start_type = type_dict[row['s']]
-        start_abbrev = abbrev_dict[start_type]
-        end_type = type_dict[row['o']]
-        end_abbrev = abbrev_dict[end_type]
+    # Remove reciprocal relationships
+    pair_list = get_pair_lists(filt_edges)
+    reciprocal_types = find_reciprocal_relations(pair_list)
+    filt_edges = remove_reciprocals(filt_edges, reciprocal_types)
 
-        # Example 'Compound' -physically interacts with-> 'Protein' becomes:
-        # 'physically-interacts-with_CpP'
-        return row['pLabel'].replace(' ', '-') + '_'+ start_abbrev + row['pLabel'][0] + end_abbrev
+    return filt_edges
 
-    edge_out = pd.DataFrame()
-    edge_out[':START_ID'] = filt_edges['s'].apply(qt.id_from_uri)
-    edge_out[':END_ID'] = filt_edges['o'].apply(qt.id_from_uri)
-    edge_out[':TYPE'] = filt_edges.apply(get_edge_type, axis = 1)
 
-    return edge_out
+def get_abbrev_dict(edge_df):
+    # Create an abbreviation dict for fast mapping of nodes and edges
+    node_types = list(set(edge_df['type']))
+    edge_types = list(set(edge_df['pLabel'].str.replace(' ', '-')))
+
+    node_abbrevs = [''.join([w[0].upper() for w in t.split(' ')]) for t in node_types]
+    edge_abbrevs = [''.join([w[0].lower() for w in t.split('-')]) for t in edge_types]
+
+    node_abbrev_dict = {t:a for t,a in zip(node_types, node_abbrevs)}
+    edge_abbrev_dict = {t:a for t,a in zip(edge_types, edge_abbrevs)}
+
+    return {**node_abbrev_dict, **edge_abbrev_dict}
+
+
+def get_metaedge_tuples():
+    pass
 
