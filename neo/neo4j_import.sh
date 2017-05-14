b1870a2b276eb1dad4024279af6372a3d791afb9
Mike Mayers <mmayers@scripps.edu>
2017-05-14 11:49:41 -0700

    Permuted networks and imported to neo4j

--- neo/neo4j_import.sh
+++ neo/neo4j_import.sh
@@ -1,5 +1,5 @@
 VERSION="neo4j-community-3.1.3"
-DB_NAME="wikidata-v0.1"
+DB_NAME="wikidata-v1.0"
 
 declare -a suffixes=("" "_perm-1" "_perm-2" "_perm-3" "_perm-4" "_perm-5")
 
@@ -7,10 +7,10 @@ for suffix in "${suffixes[@]}"; do
 
     BIN_LOC="${VERSION}_$DB_NAME$suffix/bin/neo4j-admin"
 
-    CSV_NAME="import/hetnet_baseedges$suffix"
+    CSV_NAME="import/hetnet_edges$suffix"
 
     REPORT_NAME="network${suffix}_import.report"
 
-    ./$BIN_LOC import --nodes "import/hetnet_basenodes.csv" --relationships "${CSV_NAME}.csv" --report-file="$REPORT_NAME"
+    ./$BIN_LOC import --nodes "import/hetnet_nodes.csv" --relationships "${CSV_NAME}.csv" --report-file="$REPORT_NAME"
 
 done
